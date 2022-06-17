835d6cce77d929ba3d29a74a8904bdceff3d5068
Camila Belen <camilabelencapua@gmail.com>
2022-06-17 01:36:15 -0300

    Formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,133 @@
+<template>
+  <section class="src-components-formulario-av">
+    <div class="jumbotron">
+      <h3 class="formulario">Formulario vue-form</h3>
+      <hr>
+      <hr>
+      <br>
+
+      <vue-form :state="formState" @submit.prevent="enviar()">
+    
+        <validate tag="div">
+          <label for="nombreCompleto">Nombre Completo</label>
+          <input 
+            type="text"
+            id="nombreCompleto"
+            name="nombreCompleto" 
+            class="form-control"
+            autocomplete="off"
+            v-model.trim="datos.nombreCompleto" 
+            required 
+            :minlength="nombreMinLength"
+            :maxlength="nombreMaxLength"
+            no-espacios
+          />
+
+          <field-messages name="nombreCompleto" show="$dirty">
+            <div slot="required" class="alert alert-danger mt-1">Campo requerido</div>
+            <div slot="minlength" class="alert alert-danger mt-1">
+              Este campo requiere como mínimo {{nombreMinLength}} caracteres.
+            </div>
+            <div slot="maxlength" class="alert alert-danger mt-1">
+              Este campo requiere como maximo {{nombreMaxLength}} caracteres.
+            </div>
+          </field-messages>
+        
+        </validate>
+    
+        <validate tag="div">
+          <label for="edad">Edad</label>
+          <input 
+            type="number"
+            id="edad"
+            name="edad" 
+            class="form-control"
+            autocomplete="off"
+            v-model.number="datos.edad" 
+            required 
+            :min="edadMin"
+            :max="edadMax"
+          />
+
+          <field-messages name="edad" show="$dirty">
+            <div slot="required" class="alert alert-danger mt-1">Campo requerido</div>
+            <div slot="min" class="alert alert-danger mt-1">
+              La edad mínima permitida es de {{edadMin}} años.
+            </div>
+            <div slot="max" class="alert alert-danger mt-1">
+              La edad máxima permitida es de {{edadMax}} años.
+            </div>
+          </field-messages>
+        </validate>
+        <br>
+
+        <validate tag="div">
+          <label for="email">Email</label>
+          <input 
+            type="email"
+            id="email"
+            name="email" 
+            class="form-control"
+            autocomplete="off"
+            v-model.trim="datos.email" 
+            required 
+          />
+
+          <field-messages name="email" show="$dirty">
+            <div slot="required" class="alert alert-danger mt-1">Campo requerido</div>
+            <div slot="email" class="alert alert-danger mt-1">Email no válido</div>
+          </field-messages>
+        </validate>
+        <br>
+
+        <button class="btn btn-success my-4" :disabled="formState.$invalid">Enviar</button>
+      </vue-form>      
+
+      <hr>
+
+    </div>
+  </section>
+</template>
+
+<script lang="js">
+  export default {
+      name: 'src-components-formulario',
+      components: {},
+      props: [],
+      data () {
+        return {
+          formState : {},
+          datos : this.getInicialData(),
+          nombreMinLength : 5,
+          nombreMaxLength : 15,
+          edadMin: 18,
+          edadMax: 120
+        }
+      },
+      computed: {
+    
+      },
+      mounted () {
+    
+      },
+      methods: {
+        getInicialData() {
+          return {
+            nombreCompleto: null,
+            edad: null,
+            email: null
+          }
+        },
+        enviar() {
+          console.log({...this.datos})
+          this.datos = this.getInicialData()
+          this.formState._reset()  
+        }
+      }
+    }
+</script>
+
+
+<style scoped lang="css">
+
+</style>
